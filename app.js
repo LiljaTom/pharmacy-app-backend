5b70cb762368bc8540b8430f2c34774673b382c6
Tomi Lilja <tomi.lilja@helsinki.fi>
2020-08-07 15:19:12 +0300

    Add login functionality

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const middleware = require('./utils/middleware')
 // Routers
 const productsRouter = require('./controllers/products')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 
 logger.info('connecting to database')
@@ -28,6 +29,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/products', productsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
